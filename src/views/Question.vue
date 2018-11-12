0879b03e992845a565308adee127a1975f6783b8
ktr0328 <zf7heatpa@gmail.com>
2018-11-12 12:22:33 +0900

    feat: Question Area updated

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,9 +1,33 @@
 <template>
-  <v-container fluid>
-    <h1>Question</h1>
+  <v-container fluid style='padding: 0; height: 90%'>
+    <v-toolbar dark>
+      <v-toolbar-title>Question: {{ question.title }}</v-toolbar-title>
+    </v-toolbar>
+
+    <v-layout row wrap fill-height>
+      <v-flex xs4>
+        question
+      </v-flex>
+      <v-flex xs3>
+        playground
+      </v-flex>
+      <v-flex xs5>
+        components
+      </v-flex>
+    </v-layout>
+    <v-footer absolute height="20%">
+      bottom
+    </v-footer>
+
+    <!-- <v-content>
+      <v-container fluid>
+        {{ question.text }}
+      </v-container>
+    </v-content> -->
+    <!-- <h1>Question</h1>
     <p>id: {{ question.id }}</p>
     <p>title: {{ question.title }}</p>
-    <p>text: {{ question.text }}</p>
+    <p>text: {{ question.text }}</p> -->
   </v-container>
 </template>
 
